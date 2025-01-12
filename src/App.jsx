3e41159dedcdae7968db66d1895b0ded243f968c
pornima25s <sadanshivsudhir794@gmail.com>
2025-01-12 23:35:05 +0530

    fixed bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,44 @@
-import { useState } from 'react'
+import React, { useState, useEffect } from "react";
+import Navbar from "./components/Navbar/Navbar";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Cart from "./pages/Cart/Cart";
+import PlaceOrder from "./pages/PlaceOrder/PlaceOrder";
+import Footer from "./components/Footer/Footer";
+import LoginPopup from "./components/LoginPopup/LoginPopup";
 
+const App = () => {
+  const [showLogin, setShowLogin] = useState(false);
+  const [theme, setTheme] = useState("light");
 
-function App() {
-  const [count, setCount] = useState(0)
+  useEffect(() => {
+    // Apply the theme to the body element
+    document.documentElement.setAttribute("data-theme", theme);
+  }, [theme]);
+
+  const toggleTheme = () => {
+    setTheme((prev) => (prev === "light" ? "dark" : "light"));
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      {showLogin ? <LoginPopup setShowLogin={setShowLogin} /> : <></>}
+      <div className="app">
+        <Navbar setShowLogin={setShowLogin} toggleTheme={toggleTheme} />
+        <button onClick={toggleTheme} className="theme-toggle" aria-label="Toggle Theme">
+              <span className="toggle-circle">
+             {theme === "dark" ? "🌙" : "☀️"}
+           </span>
+</button>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/order" element={<PlaceOrder />} />
+        </Routes>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
